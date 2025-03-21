448b61c3fea3aef160fb906afe80c3ed94e8a2f1
AmAkAtch <gadhavirushiraj7@gmail.com>
2025-03-21 12:31:34 +0530

    Updated REgistration and Login code for better authentication and tested their runing

--- chat-app-backend/routes/auth.js
+++ chat-app-backend/routes/auth.js
@@ -1,36 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 //register
-router.post("/register", async(req, res)=>{
-    try{
-        const {userName, password} = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({userName, password: hashedPassword});
-        await newUser.save();
-        res.status(200).json({message:"User Registration successful"});
-    }catch(err){
-        res.status(400).json({message:"Registration Failed"});
-    };
-})
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const existingUser = await User.findOne({ username });
+    console.log(existingUser.username);
+    if (existingUser)
+      return res.status(400).json({ message: "User Already Exists" });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+    res.status(200).json({ message: "User Registration successful" });
+  } catch (err) {
+    res.status(400).json({ message: "Registration Failed", Error: err });
+  }
+});
 
 //post
-router.post("/login", async(req,res)=>{
-    try{
-        const {userName, password} = req.body;
-        const user = await User.findOne(userName);
-        if(!user) return res.status(404).json({message:"User not found"});
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if(!isPasswordValid) return res.status(500).json({message:"Password Wrong"});
-        const token = jwt.sign({userId:user._id}, process.env.JWT_SECRET, {expiresIn:"1h"});
-        res.status(200).json({token});
-    }catch(err){
-        res.status(400).json({message:"Failed to login"});
-    };
-})
+router.post("/logino", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ message: "User not found" });
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.status(500).json({ message: "Password Wrong" });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(400).json({ message: "Failed to login" });
+  }
+});
 
 module.exports = router;
--- chat-app-backend/server.js
+++ chat-app-backend/server.js
@@ -1,38 +1,35 @@
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
-const { Socket } = require("socket.io");
+const { Server } = require("socket.io");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const authRoutes = require("./routes/auth")
+const authRoutes = require("./routes/auth");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Socket(server, {
-    cors:{origin:"*"}
+const io = new Server(server, {
+  cors: { origin: "*" },
 });
 
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use(authRoutes);
+app.use("/api/auth", authRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser:true,
-    useUnifiedTopology: true
-})
-.then(()=>console.log("DB connection successful"))
-.catch(err=>console.log("DB connection failed", err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connection successful"))
+  .catch((err) => console.log("DB connection failed", err));
 
-io.on("connection", (socket)=>{
-    console.log("User has joined:", socket.id);
-    socket.on("disconnect", ()=>{
-        console.log("User Disconnected:", socket.id);
-    });
-})
-
+io.on("connection", (socket) => {
+  console.log("User has joined:", socket.id);
+  socket.on("disconnect", () => {
+    console.log("User Disconnected:", socket.id);
+  });
+});
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=>{
-    console.log(`Server starter on Port ${PORT}`);
-})
+server.listen(PORT, () => {
+  console.log(`Server starter on Port ${PORT}`);
+});
